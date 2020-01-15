2714a951b3ac638972ce4d4d5016006e425cd521
manas-personatech <manas@personatech.com>
2020-01-15 15:54:27 +0530

    removed unused configs

--- src/leaflet/config.js
+++ src/leaflet/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    allFloorLevelUrl : 'http://c6b25107.ngrok.io/public/imagemap/getAll',
+    allFloorLevelUrl : 'http://ec05f0cc.ngrok.io/public/imagemap/getAll',
     defaultImageUrl : '/public/FLOORPLAN10.jpg',
     imageUrlbaseName : '/public/FLOORPLAN'
 };
